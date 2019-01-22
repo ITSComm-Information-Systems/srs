bfc072d53ef495e2830f3d83e6b93650b531d922
David P Jamison <djamison@umich.edu>
2019-01-22 14:22:10 -0500

    add role to admin module

--- apps/oscauth/models.py
+++ apps/oscauth/models.py
@@ -8,7 +8,17 @@ class ExternalModel(models.Model):
 class Role(models.Model):
     role = models.CharField(primary_key=True, max_length=30, db_column='role')
     role_description = models.CharField(max_length=2000)
+    is_dept_manager = models.CharField(max_length=1)
+    can_add_proxy = models.CharField(max_length=1)
+    can_add_nonproxy_users = models.CharField(max_length=1)
+    can_submit_orders = models.CharField(max_length=1)
+    can_run_reports = models.CharField(max_length=1)
     
+    list_display = ('first_name', 'last_name')
+    
+    def __str__(self):
+        return self.role
+
     class Meta(ExternalModel.Meta):
         db_table = 'PINN_CUSTOM\".\"UM_OSC_VALID_ROLES'
         #ordering = ['account', 'charge_group', 'description']
