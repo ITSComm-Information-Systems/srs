073a9ab397b85f683ff40254265d903eae7dac5e
David P Jamison <djamison@umich.edu>
2019-01-22 14:22:10 -0500

    add role to admin module

--- apps/oscauth/admin.py
+++ apps/oscauth/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Role
 
-admin.site.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    #model = Role
+    list_display = ['role', 'role_description','is_dept_manager','can_add_proxy',
+                   'can_add_nonproxy_users','can_submit_orders','can_run_reports']
+
+admin.site.register(Role, RoleAdmin)
+
