c13548d44d1b6b09b1465afafcb6347cafb9f5b0
David P Jamison <djamison@umich.edu>
2019-01-22 11:16:54 -0500

    add oracle

---
+++ apps/oscauth/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class ExternalModel(models.Model):
+  class Meta:
+    managed = False
+    abstract = True
+
+class Role(models.Model):
+    role = models.CharField(primary_key=True, max_length=30, db_column='role')
+    role_description = models.CharField(max_length=2000)
+    
+    class Meta(ExternalModel.Meta):
+        db_table = 'PINN_CUSTOM\".\"UM_OSC_VALID_ROLES'
+        #ordering = ['account', 'charge_group', 'description']
+
+
+class DBRouter(object):
+  def db_for_read(self, model, **hints):
+
+    if model._meta.db_table == 'PINN_CUSTOM"."UM_ECOMM_DEPT_UNITS_REPT':
+      return 'pinnacle'
+    elif model._meta.db_table == 'PINN_CUSTOM"."UM_AUTHORIZED_DEPT_USERS':
+      return 'pinnacle'
+    return 'default'
+
+  def db_for_write(self, model, **hints):
+    return 'default'
+
+  def allow_migrate(self, db, app_label, **hints):
+    if db == "pinnacle":
+      return False
+    else:
+      return True
