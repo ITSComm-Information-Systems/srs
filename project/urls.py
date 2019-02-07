2bdee5bb4da151d3a6e7d33eef1e44cc3026ca4c
David P Jamison <djamison@umich.edu>
2019-02-07 17:57:56 -0500

    Add URL paths for all app stubs

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
 
+admin.AdminSite.site_header = 'OSC Administration'
+admin.AdminSite.site_title = 'OSC Site Admin'
+
 urlpatterns = [
+    path('order/', include('order.urls')),
+    path('pages/', include('pages.urls')),
+    path('oscauth/', include('oscauth.urls')),
+    path('wismo/', include('wismo.urls')),
     path('admin/', admin.site.urls),
 ]
 
