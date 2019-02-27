76c87b515b38906546fae673a06377f41fbe3935
David P Jamison <djamison@umich.edu>
2019-02-27 08:53:42 -0500

    Add route.yaml

--- order/views.py
+++ order/views.py
@@ -11,3 +11,13 @@ def index(request):
         'item_list': item_list,
     }
     return HttpResponse(template.render(context, request))
+
+
+def phone(request):
+    #item_list = Item.objects.order_by('-id')
+    template = loader.get_template('order/order_phone.html')
+    context = {
+        'title': 'Request Service',
+        #'item_list': item_list,
+    }
+    return HttpResponse(template.render(context, request))
