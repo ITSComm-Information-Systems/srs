1986d6a94e5bad6d5d0c3299b5b2f1fcc39cf9ed
David P Jamison <djamison@umich.edu>
2019-03-04 15:31:22 -0500

    integrate impersonate

--- oscauth/urls.py
+++ oscauth/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('dept/<str:dept_id>', views.dept, name='dept'),
     path('user/<str:user_id>', views.user, name='user'),
+
+    # Switch User
+    path('su/', views.su_logout, name="su_logout"),
+    path('su_login/', views.su_login, name="su_login"),
+    path('?P<user_id>', views.login_as_user, name="login_as_user"),
+
 ]
