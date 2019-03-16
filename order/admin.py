a9fb25fc751754cec70088c21df5f2e068565aac
djamison <djamison@umich.edu>
2019-03-16 12:03:23 +0000

    Add models to order for workflow configuration

--- order/admin.py
+++ order/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
 
 
-#from .models import Service, Action, Attribute
+from .models import Service, Action, Product, Workflow, Step
 
-#class ServiceAdmin(admin.ModelAdmin):
-#    list_display  = ['name','description']
+class ProductAdmin(admin.ModelAdmin):
+    list_display  = ['name','display_seq_no','description','picture']
 
+class ActionAdmin(admin.ModelAdmin):
+    list_display  = ['name','display_seq_no']
 
-#class ActionAdmin(admin.ModelAdmin):
-#    list_display  = ['service', 'name','description']
+class ServiceAdmin(admin.ModelAdmin):
+    list_display  = ['name','display_seq_no','active','workflow']
 
+class WorkflowAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
-#class AttributeAdmin(admin.ModelAdmin):
-#    list_display  = ['name','description']
+class StepAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
-
-#admin.site.register(Service, ServiceAdmin)
-#admin.site.register(Action, ActionAdmin)
-#admin.site.register(Attribute, AttributeAdmin)
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Action, ActionAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Workflow, WorkflowAdmin)
+admin.site.register(Step, StepAdmin)
