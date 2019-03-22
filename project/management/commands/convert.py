bbffe13ac5502faab682cae3f26af9012390ac62
Faith <mafaith@UMICH.EDU>
2019-03-22 09:08:32 -0400

    Completed convert.py

--- project/management/commands/convert.py
+++ project/management/commands/convert.py
@@ -19,12 +19,23 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user_list = PinnAuthUser.objects.order_by('uniqname')
+        user_count = PinnAuthUser.objects.count()
         distinct_users = set(user_list)
+        distinct_user_count = len(distinct_users)
+        user_add_count = 0
+        user_not_added_count = 0
+        auth_added_count = 0
+        auth_not_added_count = 0
 
         for row in distinct_users:
-            print('Add/Update User: %s' % row.uniqname)
-            osc_user = upsert_user(row.uniqname)
-
+            try:
+#                print('Add/Update User: %s' % row.uniqname)
+                osc_user = upsert_user(row.uniqname)
+                user_add_count = user_add_count + 1
+            except:
+                user_not_added_count = user_not_added_count + 1
+                print('User failed MCommunity check: %s' % row.uniqname)
+       
         for row in user_list:
             try:
                 print('Add Security for: %s' % row.uniqname)
@@ -34,7 +45,17 @@ class Command(BaseCommand):
                 new_record.group = Group.objects.get(name=row.role)
                 new_record.dept = row.deptid
                 new_record.save()
+                auth_added_count = auth_added_count + 1
             except:
-                print("User: %s not found" % row.uniqname)
-
-
+                auth_not_added_count = auth_not_added_count + 1
+                print("Unable to add AuthUserDept for User: %s  Role: %s  Dept: %s" % (row.uniqname, row.role, row.deptid))
+ 
+        print('-------------------------------------------')
+        print('PinnAuthUser records read: %s' % user_count) 
+        print('Distinct users read: %s' % distinct_user_count)
+        print('Users added or updated: %s' % user_add_count) 
+        print('Users not added: %s' % user_not_added_count) 
+        print('Records added to AuthUserDept: %s' % auth_added_count) 
+        print('Records not added to AuthUserDept: %s' % auth_not_added_count) 
+        print('-------------------------------------------')
+ 
