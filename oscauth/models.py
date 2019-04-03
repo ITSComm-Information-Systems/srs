ff0a4eb6d9102113e95f209030de555bca6f401e
Faith <mafaith@UMICH.EDU>
2019-04-03 10:02:14 -0400

    Yet another attempt on Role

--- oscauth/models.py
+++ oscauth/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User, Group, get_username
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import get_username
 
 
 class Role(models.Model):  
