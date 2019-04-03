57680483f22031e21698f412388b9098eb2c15d8
Faith <mafaith@UMICH.EDU>
2019-04-03 09:55:15 -0400

    Another attempt to get username for Role

--- oscauth/models.py
+++ oscauth/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User, Group, user_logged_in
+from django.contrib.auth.models import User, Group, get_username
 
 
 class Role(models.Model):  
