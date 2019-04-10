12e6644f60a36a62e9a6c4341c173ae7d8ef3711
Faith <mafaith@UMICH.EDU>
2019-04-10 08:47:49 -0400

    Add improvements to mypriv , start of deptpriv, and addtions to pinnmodels

--- oscauth/views.py
+++ oscauth/views.py
@@ -14,9 +14,11 @@ from django.shortcuts import get_object_or_404, render
 from django.contrib.auth import get_user_model
 
 from .models import AuthUserDept
-from .models import Role, Group
+from .models import Role, Group, User
 from .forms import UserSuForm, AddUserForm
 from .utils import su_login_callback, custom_login_action, upsert_user
+from project.pinnmodels import UmOscDeptProfileV, UmCurrentDeptManagersV
+from oscauth.forms import *
 
 
 def get_name(request, parm=1):
@@ -71,27 +73,84 @@ def mypriv(request):
     depts = AuthUserDept.objects.filter(user=request.user.id).order_by('dept')
     rows = []
     prev_dept = ''
+    dept_name = ''
     group_name = ''
     col1 = ''
+    col2 = ''
     roles = ''
     i = 0
     for dept in depts:
+        dept_info = UmOscDeptProfileV.objects.filter(deptid=dept.dept)
+        print('Dept: %s' % dept.dept)
+        if dept.dept == 'All':
+            dept_name = 'All departments'
+        else:
+            for name in dept_info:
+                dept_name = name.dept_name
+        print('Name: %s' % name.dept_name)
+
         group_name = Group.objects.get(name=dept.group).name
+
         if dept.dept == prev_dept:
             roles = roles + ', ' + group_name
         else:
             if prev_dept != '':
-                data = {'col1' : col1, 'roles': roles}
+                data = {'col1' : col1, 'col2' : col2,  'roles': roles}
                 rows.append(data)
             i = i + 1
             col1 = dept.dept
+            col2 = dept_name
             roles = group_name
         prev_dept = dept.dept
-    data = {'col1' : col1, 'roles': roles}
+    data = {'col1' : col1, 'col2' : col2, 'roles': roles}
     rows.append(data)
     template = loader.get_template('oscauth/mypriv.html')
     context = {
         'title': 'My Privileges: ' + request.user.username,
+        'rows': rows
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def deptpriv(request):
+    dept_parm = '481091'
+    dept_info = UmCurrentDeptManagersV.objects.filter(deptid=dept_parm)
+    users = AuthUserDept.objects.filter(dept=dept_parm).order_by('group','user__last_name','user__first_name')
+    rows = []
+    prev_user = ''
+    group_name = ''
+    col1 = ''
+    col2 = ''
+    roles = ''
+    i = 0
+    for dept in dept_info:
+        deptid = dept.deptid
+        dept_name = dept.dept_name
+        dept_mgr_name = dept.dept_mgr_name
+        dept_mgr_uniqname = dept.dept_mgr_uniqname
+
+    for user in users:
+        last_name = User.objects.get(username=user.user).last_name
+        first_name = User.objects.get(username=user.user).first_name
+        group_name = Group.objects.get(name=user.group).name
+        if user.user == prev_user:
+            roles = roles + ', ' + group_name
+        else:
+            if prev_user != '':
+                data = {'col1' : col1, 'col2' : col2, 'roles': roles}
+                rows.append(data)
+            i = i + 1
+            col1 = user.user
+            col2 = last_name + ', ' + first_name
+            roles = group_name
+        prev_user = user.user
+    data = {'col1' : col1, 'col2' : col2, 'roles': roles}
+    rows.append(data)
+    template = loader.get_template('oscauth/deptpriv.html')
+    context = {
+        'title': 'Access For Department: ' + dept_parm + ' - '+ dept_name ,
+        'subtitle': 'Department Manager: ' + dept_mgr_name + ' (' + dept_mgr_uniqname + ')',
         'rows': rows
     }
     print(len(rows))
