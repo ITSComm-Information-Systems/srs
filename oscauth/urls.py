c763825f432bd17d36643828de982977a6e12a4a
Faith <mafaith@UMICH.EDU>
2019-05-02 10:53:13 -0400

    Work on Manage Access

--- oscauth/urls.py
+++ oscauth/urls.py
@@ -16,6 +16,15 @@ urlpatterns = [
     path('get_dept/', views.get_dept),
     path('deptpriv/<str:dept_parm>/', views.deptpriv),
     path('deptpriv/', views.deptpriv),
+    path('get_uniqname/', views.get_uniqname),
+    path('setpriv/<str:uniqname_parm>/<str:last_name>/<str:first_name>/', views.setpriv),
     path('setpriv/', views.setpriv),
+    path('showpriv/<str:uniqname_parm>/<str:last_name>/<str:first_name>/', views.showpriv),
+    path('showpriv/', views.showpriv),
+    path('addpriv/<str:uniqname_parm>/<str:last_name>/<str:first_name>/', views.addpriv),
+    path('addpriv/<str:uniqname_parm>/', views.addpriv),
+    path('addpriv/', views.addpriv),
+    path('removepriv/<str:uniqname_parm>/', views.removepriv),
+    path('removepriv/', views.removepriv),
 
 ]
