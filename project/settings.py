0e230785b9fcf6059ed844a3e3cd930408f8d0ef
David P Jamison <djamison@umich.edu>
2019-05-17 14:40:46 -0400

    move media to persistent.

--- project/settings.py
+++ project/settings.py
@@ -184,7 +184,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media' # Use persistent volume in openshift
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
