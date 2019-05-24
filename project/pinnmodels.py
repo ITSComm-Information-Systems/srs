1ff06bb3e3811010e1ce3fad5b725fcfbe887b44
dyangz <dyangz@umich.edu>
2019-05-24 11:26:19 -0400

    using the new pinnmodel

--- project/pinnmodels.py
+++ project/pinnmodels.py
@@ -506,7 +506,7 @@ class UmOscDeptUnitsRept(models.Model):
      month = models.CharField(max_length=2)
      deptid = models.CharField(max_length=6) 
      dept_descr = models.CharField(max_length=30)
-     dept_grp = models.CharField(max_length=20) 
+     dept_grp = models.CharField(primary_key= True,max_length=20) 
      dept_grp_descr = models.CharField(max_length=30)
      dept_grp_vp_area = models.CharField(max_length=20) 
      dept_grp_vp_area_descr = models.CharField(max_length=30)
