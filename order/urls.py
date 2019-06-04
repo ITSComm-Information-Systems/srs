3806559c69d6483ef1bb17f62d3b56b12c7a5898
David P Jamison <djamison@umich.edu>
2019-06-04 09:24:02 -0400

    Add custom permissions and enforce them.

--- order/urls.py
+++ order/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from order.views import Services, Workflow
 
 from . import views
 
@@ -6,6 +7,6 @@ urlpatterns = [
     path('cart/', views.get_cart),
     path('addtocart/', views.add_to_cart),
     path('submit/', views.submit_order),
-    path('services/', views.get_services),
-    path('wf/<int:action_id>', views.get_workflow),
+    path('services/', Services.as_view()),
+    path('wf/<int:action_id>', Workflow.as_view()),
 ]
