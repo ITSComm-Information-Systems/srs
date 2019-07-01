01b58f2e5cd24677784bc7d5a0ae1a884d69db68
dyangz <dyangz@umich.edu>
2019-07-01 14:19:36 -0400

    ajax

--- tools/voip/urls.py
+++ tools/voip/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path(r'', views.get_voip),
+    path('jack/', views.get_jack),
+    url(r'', views.get_voip),
+    
 ]
