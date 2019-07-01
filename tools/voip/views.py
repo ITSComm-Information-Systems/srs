bace555942fdbf99aff4bff32485ec7d16e43bf7
dyangz <dyangz@umich.edu>
2019-07-01 14:19:36 -0400

    ajax

--- tools/voip/views.py
+++ tools/voip/views.py
@@ -1,6 +1,6 @@
 import warnings
 
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect, Http404, JsonResponse
 from django.template import loader
 
 from django.contrib.auth.models import User, Group
@@ -22,23 +22,41 @@ from project.pinnmodels import UmOscLocationsInUseV, UmOSCCampusBuildingV,  UmOs
 def get_voip(request):
     submit = False
     template = loader.get_template('voip.html')
-    phone_number = request.POST.get('number', None)
+    phone_number = request.GET.get('number', None)
     current = UmOscLocationsInUseV.objects.filter(service_number__exact = phone_number).order_by('room').values_list().distinct()
     building_list = UmOscAvailableLocsV.objects.values_list('building_id', 'building_name').distinct()
+    selected = ''
+    choice = ''
     if phone_number!= None:
         submit = True
-    selected = ''
+    if request.is_ajax():
+        choice = request.GET.get('jacks',None)
+        selected = UmOscLocationsInUseV.objects.filter(service_number__exact = phone_number, jack__exact = choice).order_by('room').values_list().distinct()
+        phone_number = '123489y128943'
     context = {
         'title': 'VOIP Location Change',
         'phone_number': phone_number,
         'current': list(current),
-        'building_list': building_list,
+        'building_list': list(building_list),
         'submit': submit,
-        'selected': selected,
+        'selected': list(selected),
+        'choice':choice,
+        
 
     }
+    if request.is_ajax():
+        return JsonResponse(context)
     return HttpResponse(template.render(context,request))
-
-# def selected_jack(request):
-#     selected = request.GET.get('jacks', None)
+    #return JsonResponse(context)
     
+def get_jack(request): # need to get this json working then we should be good
+    phone_number = request.GET.get('number', None)
+    choice = request.GET.get('jacks',None)
+    selected = UmOscLocationsInUseV.objects.filter(jack__exact = choice).order_by('room').values_list().distinct()
+    template = loader.get_template('voip.html')
+    data = {
+        'selected': selected,
+        'choice':choice,
+
+    }
+    return JsonResponse(data)
