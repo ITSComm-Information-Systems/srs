c4ef659b95414172d44c503da9aa193425d5908f
schoepke <schoepke@umich.edu>
2019-07-01 16:07:02 -0400

    Detail of Charges front end

--- reports/doc/urls.py
+++ reports/doc/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	path('report/tsr/', views.show_tsr),
+	path('report/detail/', views.show_detail),
+	path('report/', views.generate_report),
     path('', views.get_doc),
     
 ]
