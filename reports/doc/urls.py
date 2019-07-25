c2232a754d84fffd4a02d1e3989a3fb1396f76d1
schoepke <schoepke@umich.edu>
2019-07-25 13:41:06 -0400

    need user ID types

--- reports/doc/urls.py
+++ reports/doc/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('select-cf/', views.select_cf),
 	path('report/detail/', views.show_detail),
 	path('report/', views.generate_report),
-    path('', views.get_doc),
+    path('', views.get_doc, name='base_doc'),
     
 ]
