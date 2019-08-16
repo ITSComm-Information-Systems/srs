bb236226c894d7f93f05782b5f98b304ed5771ed
frga <frga@umich.edu>
2019-08-16 10:20:04 -0400

    fixing tolls

--- reports/toll/views.py
+++ reports/toll/views.py
@@ -68,7 +68,7 @@ def generate(request):
 	dept = UmOscDeptProfileV.objects.filter(deptid=dept_id)
 	dept_name = dept[0].deptid
 	dept_mgr = dept[0].dept_mgr
-	dept_mgr_uniq = dept_info[0].dept_mgr_uniqname
+	dept_mgr_uniq = dept[0].dept_mgr_uniqname
 
 
 	inactive = False
