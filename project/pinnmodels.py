1627029118c800190b716c0e14e7983484ab80ee
mayakh <mayakh@umich.edu>
2019-11-14 15:55:06 -0500

    Added test model in project

--- project/pinnmodels.py
+++ project/pinnmodels.py
@@ -6,9 +6,20 @@
 #   * Kept `managed = False` lines if you wish to not  allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.auth.models import User
 
 # This view uses the Pinnacle location table and includes locations added by ITS staff
 #  as well as the official builfing codes from MPathways
+class Test(models.Model):
+   username = models.ForeignKey(User, on_delete=models.CASCADE)
+   url  = models.CharField(max_length=500)
+   result = models.IntegerField(default=200)
+
+   def __str__(self):
+        return self.username
+   class Meta:
+        verbose_name_plural = "Tests"
+
 class UmOSCBuildingV(models.Model):
    building_code = models.CharField(max_length=15, primary_key=True)
    building_name = models.CharField(max_length=100,null=True) 
