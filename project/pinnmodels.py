03fedefd8cc38a6cc8d0590b583171f8105cdc84
David P Jamison <djamison@umich.edu>
2019-11-19 09:58:57 -0500

    Add new table project_test, register it in admin

--- project/pinnmodels.py
+++ project/pinnmodels.py
@@ -10,15 +10,6 @@ from django.contrib.auth.models import User
 
 # This view uses the Pinnacle location table and includes locations added by ITS staff
 #  as well as the official builfing codes from MPathways
-class Test(models.Model):
-   username = models.ForeignKey(User, on_delete=models.CASCADE)
-   url  = models.CharField(max_length=500)
-   result = models.IntegerField(default=200)
-
-   def __str__(self):
-        return self.username
-   class Meta:
-        verbose_name_plural = "Tests"
 
 class UmOSCBuildingV(models.Model):
    building_code = models.CharField(max_length=15, primary_key=True)
