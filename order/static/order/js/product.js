2c730f83657de18c2f2c5123761da944a0718a56
frga <frga@umich.edu>
2020-01-28 11:59:20 -0600

    fixes 46 (I Hope)

--- order/static/order/js/product.js
+++ order/static/order/js/product.js
@@ -18,18 +18,34 @@ function filterProducts(obj) {
 		$("#mustchoose").hide();
 		$("#selecteditems").show();
 		selected_items[name] = $(card).context.value;
-		 var result = ''
-		 for(var key in  selected_items)
-			result += ('<li class="ul-'+ $(card).attr('id') + '">' + selected_items[key] + " - " + key  +'</li>');
-		document.getElementById("items_list").innerHTML = result
+		// Maya's Old code
+		//  var result = ''
+		//  for(var key in  selected_items)
+		// 	result += ('<li class="ul-'+ $(card).attr('id') + '">' + selected_items[key] + " - " + key  +'</li>');
+		// document.getElementById("items_list").innerHTML = result
+
+		// Check to see if that item has been addded already to the list as an UL element
+		var existingClass = ".ul-" ;
+		existingClass += $(card).attr('id');
+		if ($(existingClass).text() != "") {
+			// If it is, then change the text of that UL element
+			newtext = "" + selected_items[name] + " - " + name
+			$(existingClass).text(newtext);
+		}
+		// If the UL tag doesn't exist, then create add a UL into the list type
+		else {
+			var newUL = '';
+			newUL += ('<li class="ul-'+ $(card).attr('id') + '">' + selected_items[name] + " - " + name  +'</li>');
+			
+			document.getElementById("items_list").innerHTML += newUL
+		}
 	}
 	else {
 		$(card).closest('.product-card').removeClass("selected-card");
 		$(card).removeClass('selected').removeClass('btn-outline-success');
+		// If a product used be on their list, then remove it from their list
         var id = $(card).attr('id');
         $(".ul-"+id).remove();
-        console.log("here")
-        console.log($("#"+id).val());
 	} 
 
 }
