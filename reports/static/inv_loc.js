11c26ced72a6ef2057fa68bf975a2565aad0e3d0
schoepke <schoepke@umich.edu>
2020-04-01 15:56:05 -0400

    all but date filter converted to new format

--- reports/static/inv_loc.js
+++ reports/static/inv_loc.js
@@ -13,6 +13,38 @@ $(document).ready(function() {
     $('#date-filter').hide();
     $('#filterremove').hide();
 
+    // Pre-register filter-able columns
+    locCols = [];
+    typeCols = [];
+    cfCols =[];
+    lt12Cols = [];
+    gt6Cols = [];
+
+    $('.invLocTable tr.datarow').each(function() {
+        locCols.push($(this).find('.location'));
+        typeCols.push($(this).find('.type'));
+        cfCols.push($(this).find('.chartfield'));
+        lt12Cols.push($(this).find('.lt12'));
+        gt6Cols.push($(this).find('.gt6'));
+    })
+    // var locCols = $(),
+    //     typeCols = [],
+    //     cfCols = $(),
+    //     lt12Cols = $(),
+    //     gt6Cols = $();
+
+    // $('.invLocTable tr.datarow').each(function() {
+    //     var columns = $(this).find('td');
+
+    //     locCols.add(columns.eq('.location'));
+    //     console.log(columns.eq('.type'));
+    //     typeCols.push(columns.eq('.type'));
+    //     cfCols.add(columns.eq('.chartfield'));
+    //     lt12Cols.add(columns.eq('.lt12'));
+    //     gt6Cols.add(columns.eq('.gt6'));
+    // });
+
+
     // Apply filters on click
     $('#filterapply').on('click', function(e) {
         e.preventDefault();
@@ -21,9 +53,11 @@ $(document).ready(function() {
         cf = $('#cf').val();
         date = $('#date').val();
 
-        var filters = [loc, type, cf, date];
+        // var filters = [loc, type, cf, date];
 
-        filter(filters, 'filter');
+        // filter(filters, 'filter');
+
+        new_filter();
 
         $('#filters-info').show();
     });
@@ -166,56 +200,182 @@ function filter(filters, action) {
         })
     }
 
-    // Additional filters
-    $('.invLocTable tr.datarow').each(function() {
-        current_cf = $(this).attr('id').split(':')[1];
+    if (location || type || date) {
+        // Additional filters
+        $('.invLocTable tr.datarow').each(function() {
+            current_cf = $(this).attr('id').split(':')[1];
 
-        // Remove totals and ability to download
-        $('#total-row'.concat(current_cf)).hide();
-        $('#download-link').hide();
+            // Remove totals and ability to download
+            $('#total-row'.concat(current_cf)).hide();
+            $('#download-link').hide();
 
-        // Get current location from table
-        current_loc = $(this).find('.location').html();
-        current_loc = current_loc.split('<br>')[0];
-        current_loc = current_loc.split('Location: ')[1];
+            var columns = $(this).find('td');
 
-        // Filter by location, if specified
-        if (location && current_loc != location) {
-            $(this).hide();
-            html = '<strong>Location: </strong>'.concat(location).concat('<br>');
-            $('#location-filter').html(html);
-            $('#location-filter').show();
-        }
+            //current_loc = columns.eq('.location').html();
 
-        // Filter by type, if specified
-        if (type && $(this).find('.type').html() != type) {
-            $(this).hide();
-            html = '<strong>User ID Type: </strong>'.concat(type).concat('<br>');
-            $('#type-filter').html(html);
-            $('#type-filter').show();
-        }
+            // Get current location from table
+            current_loc = $(this).find('.location').html();
+            current_loc = current_loc.split('<br>')[0];
+            current_loc = current_loc.split('Location: ')[1];
 
-        // Filter by last call date, if specified
-        if (date) {
-            filter_match = ((date == '6-12' && $(this).find('.lt12').html() == 1 && $(this).find('.gt6').html() == 1) ||
-                (date == '12+' && $(this).find('.lt12').html() == 0) || (date == 'lt6' && $(this).find('.gt6').html() == 0))
-
-            if (filter_match == false || $(this).find('.date').html() == 'None') {
+            // Filter by location, if specified
+            if (location && current_loc != location) {
                 $(this).hide();
-                html = '<strong>Last Call Date: </strong>'.concat(date).concat('<br>');
-                $('#date-filter').html(html);
-                $('#date-filter').show();
+                html = '<strong>Location: </strong>'.concat(location).concat('<br>');
+                $('#location-filter').html(html);
+                $('#location-filter').show();
+            }
+
+            // Filter by type, if specified
+            if (type && $(this).find('.type').html() != type) {
+                $(this).hide();
+                html = '<strong>User ID Type: </strong>'.concat(type).concat('<br>');
+                $('#type-filter').html(html);
+                $('#type-filter').show();
+            }
+
+            // Filter by last call date, if specified
+            if (date) {
+                filter_match = ((date == '6-12' && $(this).find('.lt12').html() == 1 && $(this).find('.gt6').html() == 1) ||
+                    (date == 'gt12' && $(this).find('.lt12').html() == 0) || (date == 'lt6' && $(this).find('.gt6').html() == 0))
+
+                if (filter_match == false || $(this).find('.date').html() == 'None') {
+                    $(this).hide();
+                    html = '<strong>Last Call Date: </strong>'.concat($('#'.concat(date)).html()).concat('<br>');
+                    $('#date-filter').html(html);
+                    $('#date-filter').show();
+                }
+            }
+
+
+            if (table_empty(current_cf)) {
+                $('#'.concat(current_cf)).hide();
+            }
+            else {
+                $('#'.concat(current_cf)).show();
+            }
+        })
+    }
+}
+
+function new_filter() {
+    var loc = $('#location').val(),
+    type = $('#type').val(),
+    cf = $('#cf').val(),
+    date = $('#date').val();
+
+    // Remove totals and ability to download
+    // $('#total-row'.concat(current_cf)).hide();
+    // $('#download-link').hide();
+
+    if (loc) {
+        for (i = 0; i < locCols.length; i++) {
+            current_loc = locCols[i][0].innerText;
+            if (current_loc) {
+                current_loc = current_loc.split('Location: ')[1];
+                current_loc = current_loc.split('Floor: ')[0];
+                current_loc = current_loc.replace('\n', '');
+            }
+
+            if (current_loc == loc) {
+                locCols[i].parent().hide();
+                html = '<strong>Location: </strong>'.concat(current_loc).concat('<br>');
+                $('#location-filter').html(html);
+                $('#location-filter').show();
             }
         }
+    }
+
+    if (type) {
+        for (i = 0; i < typeCols.length; i++) {
+            if (typeCols[i][0].innerText == type) {
+                typeCols[i].parent().hide();
+                html = '<strong>User ID Type: </strong>'.concat(type).concat('<br>');
+                $('#type-filter').html(html);
+                $('#type-filter').show();
+            }
+        }
+    }
+
+    if (cf) {
+        for (i = 0; i < cfCols.length; i++) {
+            console.log(cfCols[i][0].innerText);
+            if (cfCols[i][0].innerText == cf) {
+                id = '#'.concat(cf);
+                $(id).hide();
+                html = '<strong>Chartfield: </strong>'.concat(cf).concat('<br>');
+                $('#cf-filter').html(html);
+                $('#cf-filter').show();
+            }
+        }
+    }
+
+    if (date) {
+        filter_match = ((date == '6-12' && $(this).find('.lt12').html() == 1 && $(this).find('.gt6').html() == 1) ||
+                    (date == 'gt12' && $(this).find('.lt12').html() == 0) || (date == 'lt6' && $(this).find('.gt6').html() == 0))
+
+        if (filter_match == false || $(this).find('.date').html() == 'None') {
+            $(this).hide();
+            html = '<strong>Last Call Date: </strong>'.concat($('#'.concat(date)).html()).concat('<br>');
+            $('#date-filter').html(html);
+            $('#date-filter').show();
+        }
+
+                
+        for (i = 0; i < typeCols.length; i++) {
+            if (typeCols[i][0].innerText == type) {
+                typeCols[i].parent().hide();
+                html = '<strong>User ID Type: </strong>'.concat(type).concat('<br>');
+                $('#type-filter').html(html);
+                $('#type-filter').show();
+            }
+        }
+    }
+
+    // if (table_empty(current_cf)) {
+    //     $('#'.concat(current_cf)).hide();
+    // }
+    // else {
+    //     $('#'.concat(current_cf)).show();
+    // }
+
+    // // EDIT EDIT EDIT EDIT
+    // current_loc = $(this).find('.location').html();
+    // current_loc = current_loc.split('<br>')[0];
+    // current_loc = current_loc.split('Location: ')[1];
+    // locCols.add(current_loc);
 
 
-        if (table_empty(current_cf)) {
-            $('#'.concat(current_cf)).hide();
-        }
-        else {
-            $('#'.concat(current_cf)).show();
-        }
-    })
+    // if (loc) {
+    //     locCols.each(function() {
+    //         if (!this.html() == loc) {
+    //             $(this).parent().hide();
+    //         }
+    //     })
+    // }
+
+    // if (type) {
+    //     typeCols.each(function() {
+    //         if (!this == type) {
+    //             $(this).parent().hide();
+    //         }
+    //     })
+    // }
+
+    // if (cf) {
+    //     cfCols.each(function() {
+    //         if (!this == cf) {
+    //             $(this).parent().hide();
+    //         }
+    //     })
+    // }
+
+    // if (date) {
+    //     // write later
+    // }
+
+
+
 }
 
 function table_empty(cf) {
