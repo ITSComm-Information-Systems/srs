5304ee3a0561bbfa9d923efa6da83f8304f0cbba
David P Jamison <djamison@umich.edu>
2021-02-15 12:12:04 -0600

    add shortcode field

--- project/models.py
+++ project/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_shortcode(value):
+
+     if re.search("\d{6}", value):
+          print('todo lookup shortcode')
+     else:
+          raise ValidationError(f'Please enter a six digit shortcode.')
+
 
 # This view uses the Pinnacle location table and includes locations added by ITS staff
 #  as well as the official builfing codes from MPathways
@@ -12,3 +23,16 @@ class Test(models.Model):
         return self.url
    class Meta:
         verbose_name_plural = "Tests"
+
+
+class ShortCodeField(models.CharField):
+
+    description = "Six digit shortcode"
+    help_text = 'Chartfield/Chartcom'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 6
+        kwargs['validators']=[validate_shortcode]
+        kwargs['help_text']='Six digit shortcode for billing purposes.'
+
+        super().__init__(*args, **kwargs)
