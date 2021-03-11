4f4a72852653b34711389324dcb1509539dc1024
David P Jamison <djamison@umich.edu>
2021-03-11 14:08:55 -0500

    add get_ticket to mctest

--- project/management/commands/mctest.py
+++ project/management/commands/mctest.py
@@ -1,9 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 
 from oscauth.utils import get_mc_group, McGroup
 from order.models import BackupDomain, BackupNode, Item, Ticket
 from oscauth.models import LDAPGroup
 
+import requests, json
 #import datetime, csv
 
 class Command(BaseCommand):
@@ -23,5 +25,42 @@ class Command(BaseCommand):
         #ticket_list = Ticket.objects.all()
 
         #for ticket in ticket_list
-        item.route()
+        #item.route()
+        self.get_ticket()
 
+    def get_ticket(self):
+
+
+        client_id = settings.UM_API['CLIENT_ID']
+        auth_token = settings.UM_API['AUTH_TOKEN']
+        base_url = settings.UM_API['BASE_URL']
+
+        headers = { 
+            'Authorization': f'Basic {auth_token}',
+            'accept': 'application/json'
+            }
+
+
+        url = f'{base_url}/um/it/oauth2/token?grant_type=client_credentials&scope=tdxticket'
+        response = requests.post(url, headers=headers)
+        response_token = json.loads(response.text)
+        access_token = response_token.get('access_token')
+
+        attributes = []
+        for id in range(1840, 1880):
+            attributes.append({'ID': id, 'Value': id})
+
+
+
+        headers = {
+            'X-IBM-Client-Id': client_id,
+            'Authorization': 'Bearer ' + access_token,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json' 
+            }
+
+
+        #response = requests.post( base_url + '/um/it/31/tickets', data=data_string, headers=headers )
+        response = requests.get( base_url + '/um/it/31/tickets/680842', headers=headers )
+
+        print(response.text)
