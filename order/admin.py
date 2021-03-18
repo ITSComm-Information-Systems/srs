408d88b6604bfe7d71ab59086b432ddc24f4e64e
David P Jamison <djamison@umich.edu>
2021-03-18 18:14:56 -0400

    prefix & cpu

--- order/admin.py
+++ order/admin.py
@@ -145,7 +145,7 @@ class DatabaseAdmin(admin.ModelAdmin):
 
     fieldsets = (
         (None, {
-            'fields': (('name', 'in_service'), 'owner', 'shortcode', ('type','version'),
+            'fields': (('name', 'in_service'), 'owner', 'shortcode', 'type',
             ('support_email','support_phone'),
             ('shared', 'size'),
             'purpose'
