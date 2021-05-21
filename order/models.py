90cd3ffcbdc0c4e825ccdd94a2a993b447edcfa3
David P Jamison <djamison@umich.edu>
2021-05-21 16:00:26 -0400

    save database for dedicated

--- order/models.py
+++ order/models.py
@@ -1280,6 +1280,15 @@ class Item(models.Model):
 
         rec.save()
 
+        try:
+            db_type = self.data.get('database')
+            if db_type:
+                db_choice = Choice.objects.get(parent__code='DATABASE_TYPE', label=db_type)
+                db = Database.objects.update_or_create(server=rec,
+                    defaults={'name': rec.name, 'owner': rec.owner, 'support_email': rec.support_email, 'support_phone': rec.support_phone, type: db_choice, 'shortcode': rec.shortcode, 'size': 0, 'purpose': 'dedicated server'})
+        except:
+            print('error creating database')
+
         d = self.data.get('non_regulated_data')
         if d:
             print(d)
