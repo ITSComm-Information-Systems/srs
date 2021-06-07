efa2bd4ec6c587e5c10a9293fc124ccff38d5c4e
Jingcong Hu <hujingc@umich.edu>
2021-06-07 15:42:26 -0400

    specify app directories. 'DIRS': ['apps/rte/templates','apps/bom/templates'],

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,7 @@ ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['apps/rte/templates','apps/bom/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
