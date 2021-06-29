a26ed8ae61e10eb30718a6d87b2c6911f0df3372
Jingcong Hu <hujingc@umich.edu>
2021-06-29 19:21:02 -0400

    gen mbid

---
+++ apps/mbid/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from django.conf.urls import url
+from . import views
+
+app_name = 'mbid'
+urlpatterns = [
+    # Mike
+    path('create_cycle/', views.create_cycle,name='create_cycle'),
+    path('create_cycle/complete/<int:message>', views.complete),
+    path('edit_cycle/', views.edit_cycle, name='edit_cycle'),
+    path('review/', views.review, name='review'),
+    path('review/complete/<int:message>', views.complete),
+
+    # Vendor
+    path('create_report/', views.create_cycle_report,name='create_report'),
+    path('upload_bids/', views.upload_bids, name='upload_bids'),
+    path('review_bids/',views.review_bids, name='review_bids'),
+
+    # All
+    path('faq/', views.faq, name='faq'),
+    url(r'', views.home, name='home'), #keep at bottom
+
+]
