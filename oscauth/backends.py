c392c97f2fe1b7f278a96aaaa652272e527074e2
David P Jamison <djamison@umich.edu>
2021-08-03 17:09:05 -0400

    delete mozilla_django_oidc folder and subclass BE

--- oscauth/backends.py
+++ oscauth/backends.py
@@ -3,6 +3,32 @@
 from django.contrib.auth import get_user_model
 from project.pinnmodels import UmRteTechnicianV, UmBomProcurementUsersV
 
+from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+import unicodedata
+
+
+def generate_username(email):
+    print(email)
+    return unicodedata.normalize('NFKC', email).split('@')[0]
+
+
+class UMichOIDCBackend(OIDCAuthenticationBackend):
+    @staticmethod
+    def _set_claims(user, claims):
+        user.first_name = claims.get('given_name', '')
+        user.last_name = claims.get('family_name', '')
+
+    def create_user(self, claims):
+        user = super().create_user(claims)
+        self._set_claims(user, claims)
+        user.save()
+        return user
+
+    def update_user(self, user, claims):
+        self._set_claims(user, claims)
+        user.save()
+        return user
+
 
 class SuBackend(object):
     supports_inactive_user = False
