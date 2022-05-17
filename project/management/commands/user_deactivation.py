25f80e1debdc065d6d17f1d446b6dd85e15ff01d
David P Jamison <djamison@umich.edu>
2022-05-17 18:02:31 -0400

    add update parm

--- project/management/commands/user_deactivation.py
+++ project/management/commands/user_deactivation.py
@@ -2,56 +2,55 @@ from project.integrations import MCommunity
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from oscauth.models import AuthUserDept
-
-import datetime
+import csv, io, datetime
 
 class Command(BaseCommand):
     help = 'Deactivate Users'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--update')
+
     def handle(self, *args, **options):
+
+        update = False
+        if options['update']:
+            if options['update'] == 'True':
+                update = True
 
         mc = MCommunity()
         u = 0
+
+        csvfile =  open(f'/Users/djamison/Downloads/user_access.csv','w', encoding='mac_roman')
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['uniqname', 'afil', 'last login','srs groups', 'srs permissions','dept permissions'])
 
         for user in User.objects.filter(password='').order_by('username'):   # Don't select system accounts.
             mc_user = mc.get_user(user.username)
 
             if not self.is_active(mc_user, user.username):
 
-                print('    last login:', user.last_login)
-                if user.is_staff:
-                    print('    Staff')
-
-                if user.is_superuser:
-                    print('    Superuser')
-
-                if user.groups.count() > 0:
-                    print('    Groups:')
-                    for group in user.groups.all():
-                        print('     ', group)
-
-                if user.user_permissions.count() > 0:
-                    print('    Permissions:')
-                    for perm in user.user_permissions.all():
-                        print('     ', perm)
-
                 department_perms = AuthUserDept.objects.filter(user=user)
 
-                for perm in department_perms:
-                    print('   ', perm.dept, perm.group)
+                if mc_user == None:
+                    afil = 'Not in MCommunity'
+                else:
+                    afil = mc_user.umichInstRoles
 
-                print(' ')
+                csvwriter.writerow([user.username, afil, user.last_login, user.groups.count(), user.user_permissions.count(), len(department_perms)])
+
+                if update == True:
+                    department_perms.delete()
+                    self.remove_security(user)
 
             u+=1
 
-            #if u % 10 == 0:
-            #    print(datetime.datetime.now(), str(u),'records read')
+            if u % 10 == 0:
+                print(datetime.datetime.now(), str(u),'records read')
                 
         print(datetime.datetime.now(), str(u),'records updated')
 
     def is_active(self, mc_user, username):
         if mc_user == None:
-            print(username, 'not in MCommunity')
             return False
         
         for role in mc_user.umichInstRoles:
@@ -64,7 +63,6 @@ class Command(BaseCommand):
             if 'Faculty' in role:
                 return True
 
-        print(username, 'Affiliations ', mc_user.umichInstRoles)
         return False
 
     def remove_security(self, user):
@@ -76,11 +74,6 @@ class Command(BaseCommand):
         user.is_superuser = False
         user.save()
 
-        department_perms = AuthUserDept.objects.filter(user=user)
 
-        for perm in department_perms:
-            print('   ', perm.dept, perm.group)
-        
-        #print(department_perms.count(), 'depts removed')
 
 
