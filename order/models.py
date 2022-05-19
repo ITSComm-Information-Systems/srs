3e9311a489a7fdc29d9d0eb19f49e09c5220a3fd
David P Jamison <djamison@umich.edu>
2022-05-19 17:12:35 -0400

    set db flag

--- order/models.py
+++ order/models.py
@@ -1220,6 +1220,7 @@ class Item(models.Model):
                     attributes.append({'ID': 1953, 'Value': MCommunity().get_group_email_and_name(group_name)})  # Admin Group
 
                 attributes.append({'ID': 5319, 'Value': db})
+                attributes.append({'ID': 1858, 'Value': db})
                 attributes.append({'ID': 1952, 'Value': 203}) # Managed
 
                 if db == 'MSSQL':
