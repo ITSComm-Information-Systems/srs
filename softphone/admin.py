8f6351cc4507b5d2b98f17c10fd0ef20507891e7
David P Jamison <djamison@umich.edu>
2022-06-30 15:35:37 -0400

    remove category

--- softphone/admin.py
+++ softphone/admin.py
@@ -9,6 +9,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Selection)
 class SelectionAdmin(admin.ModelAdmin):
-    list_display = ['service_number','subscriber','uniqname_correct','uniqname','migrate','category','updated_by','update_date']
+    list_display = ['service_number','subscriber','uniqname_correct','uniqname','migrate','updated_by','update_date']
     ordering = ['-update_date']
     search_fields = ['service_number','uniqname','updated_by']
