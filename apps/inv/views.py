2c0697a8f9619efaf53b84218955976d267167a1
Ryan Hoffer <rhoffer@umich.edu>
2022-08-19 15:19:22 -0400

    Added a basic dumb table

--- apps/inv/views.py
+++ apps/inv/views.py
@@ -1,15 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import loader
 
-import datetime
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-
-def home(request):
-    template = loader.get_template('inv/home.html')
-    context={}
-    return HttpResponse(template.render(context, request))
+def base(request):
+    context = {'test':'Hello World'}
+    template = 'inv/base.html'
+    return render(request,template,context)
