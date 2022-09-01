6b398fdb17129cbcee6a60e8dbbf2813188d01f3
David P Jamison <djamison@umich.edu>
2022-09-01 18:59:16 -0400

    latest

--- project/management/commands/softphone_email.py
+++ project/management/commands/softphone_email.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from project.models import Email
 from project.pinnmodels import UmMpathDwCurrDepartment
 from softphone.models import Ambassador, SelectionV, next_cut_date
@@ -8,16 +8,19 @@ import csv
 
 class Command(BaseCommand):
     help = 'Send Email to Softphone Users'
+    bcc = 'itscomm.information.systems.shared.account@umich.edu'
 
     def add_arguments(self, parser):
         parser.add_argument('--file')  
         parser.add_argument('--email')  
+        parser.add_argument('--audit')  
 
     def handle(self, *args, **options):
 
         email = Email.objects.get(code=options['email'])
         cut_date = next_cut_date()
-        body = email.body.replace('{%%date%%}', cut_date.strftime('%B %-d, %Y'))
+        email.body = email.body.replace('{%%date%%}', cut_date.strftime('%B %-d, %Y'))
+
         user_query = SelectionV.objects.filter(cut_date=cut_date).values_list('uniqname', flat=True)
         user_list = []
 
@@ -27,30 +30,45 @@ class Command(BaseCommand):
                 csv_reader = csv.reader(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL)
                 for row in csv_reader:
                     user_list.append(row[0])
-        elif email.code == 'TUE_NO_LOGIN':
-            user_list = user_query.filter(zoom_login='N')
-        elif email.code == 'WED_NO_LOGIN':
-            user_list = user_query.filter(zoom_login='N')
+        elif email.code in ['TUE_NO_LOGIN','WED_NO_LOGIN']:
+            user_list = user_query.values_list('uniqname','updated_by').filter(zoom_login='N')
         elif email.code == 'USER_MIGRATE':
             user_list = user_query
         elif email.code == 'UA_WEEKLY':
-            dept_list = SelectionV.objects.filter(cut_date=cut_date).values_list('dept_id', flat=True).distinct()
-            dept_groups = list(UmMpathDwCurrDepartment.objects.filter(deptid__in=dept_list).values_list('dept_grp', flat=True).distinct())
-            amb = Ambassador.objects.filter(dept_group__in=dept_groups).values_list('user__username')
-            print(amb)
+            #TO DO ADD UA
+            email.subject = email.subject.replace('{%%date%%}', cut_date.strftime('%B %-d, %Y'))
+
+        if options['audit']:
+            print('AUDIT ONLY****')
+            for user in user_list:
+                print(user)
+            return
+
+        self.send_email(email, user_list)
+
+    def send_email(self, email, user_list, cc=[]):
+        text_message = email.subject
+
+        if settings.ENVIRONMENT != 'Production':
+            print('Non-prod, do not send to:', user_list)
+            user_list = ['djamison']
 
         for user in user_list:
-            if user:
-                user = user + '@umich.edu'
-                if settings.ENVIRONMENT == 'Production':
-                    send_mail(email.subject,'See attachment.', email.sender, [user], fail_silently=False, html_message=body)
-                    print('sent to', user)
-                else:
-                    print('audit', user)
+            if type(user)==tuple:
+                to = user[0]
+                cc = user[1]
+            else:
+                to = user
 
-        if email.cc:
-            if settings.ENVIRONMENT == 'Production':
-                send_mail(email.subject,'See attachment.', email.sender, [email.cc], fail_silently=False, html_message=body)
+            if to or cc:
+                if to:
+                    to = to + '@umich.edu'
+                if cc:
+                    cc = cc + '@umich.edu'
 
+                msg = EmailMultiAlternatives(email.subject, text_message, email.sender, [to], bcc=[self.bcc], cc=[cc])
+                msg.attach_alternative(email.body, "text/html")
+                msg.send()
 
+                print('Sent to', to, 'cc', cc)
 
