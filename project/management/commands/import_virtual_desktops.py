1beb6579dd30d0c0a1fc4de577ee3bf43346f6dc
Ryan Hoffer <rhoffer@umich.edu>
2022-12-21 03:28:26 +0000

    Update project/management/commands/import_virtual_desktops.py

--- project/management/commands/import_virtual_desktops.py
+++ project/management/commands/import_virtual_desktops.py
@@ -1,1 +1,31 @@
+from django.core.management.base import BaseCommand
+import json
+
+from myapp.models import Computer
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('json_file', type=str, help='Path to JSON file')
+
+    def handle(self, *args, **kwargs):
+        json_file = kwargs['json_file']
+
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+
+        for item in data:
+            computer = Computer(
+                id=item['id'],
+                shortcode=item['shortcode'],
+                pool=item['pool'],
+                gpu=item['gpu'],
+                memory=item['memory'],
+                cpu=item['cpu'],
+                storage=item['storage'],
+                individual_cost=item['individual_cost'],
+                num_computers=item['num_computers'],
+                total_cost=item['total_cost'],
+                group=item['group']
+            )
+            computer.save()
 
