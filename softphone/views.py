44fa88cf70c1723358bbbb5869fac73e7b6321ad
David P Jamison <djamison@umich.edu>
2023-01-30 17:03:10 -0500

    version 1

--- softphone/views.py
+++ softphone/views.py
@@ -206,6 +206,38 @@ class PauseUser(LoginRequiredMixin, View):
         return HttpResponseRedirect(f'/softphone/pause/{uniqname}')
 
 
+
+class Deskset(LoginRequiredMixin, View):
+    title = 'Request Deskset'
+    field_list = ['subscriber','service_number','subscriber_uniqname' \
+                ,'subscriber_first_name','subscriber_last_name','dept_id','migrate']
+
+    def get(self, request, dept_id):
+
+        phone_list = SelectionV.objects.filter(dept_id=dept_id, migrate='YES')
+
+        return render(request, 'softphone/deskset.html',
+                      {'title': self.title,
+                       'full_list': phone_list,
+                       'dept_list': get_department_list('2', request.user)})
+
+    def post(self, request, uniqname):
+        OptOutFormSet = formset_factory(OptOutForm, extra=0)
+        formset = OptOutFormSet(request.POST)
+
+        formset.is_valid()
+        for form in formset:
+            pause_date = form.cleaned_data.get('pause_until', 'None')
+
+            if pause_date != 'None':
+                subscriber = form.cleaned_data.get('subscriber')
+                rec = Selection.objects.get(subscriber=subscriber)
+                comment = form.cleaned_data.get('comment')
+                rec.pause(request.user, pause_date, comment)
+
+        return HttpResponseRedirect(f'/softphone/pause/{uniqname}')
+
+
 class StepSubscribers(LoginRequiredMixin, View):
 
     def post(self, request, dept_id):
