497b02470632884eb73037fb3737ccf4489dcef7
David P Jamison <djamison@umich.edu>
2023-02-03 17:33:03 -0500

    v1

--- project/admin.py
+++ project/admin.py
@@ -4,6 +4,8 @@ from django.urls import path
 from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 from django.core.management import call_command
+from django.template.response import TemplateResponse
+from django.core.mail import EmailMultiAlternatives
 
 @admin.register(Email)
 class EmailAdmin(admin.ModelAdmin):
@@ -13,7 +15,8 @@ class EmailAdmin(admin.ModelAdmin):
         urls = super().get_urls()
 
         download_url = [
-            path('<int:object_id>/send_email/', self.send_email),
+            path('<int:object_id>/send_test/', self.send_test),
+            path('<int:object_id>/send_to_user/', self.send_to_user),
         ]
         return download_url + urls
 
@@ -24,12 +27,39 @@ class EmailAdmin(admin.ModelAdmin):
             request, object_id, form_url, extra_context={'email': email},
         )
 
-    def send_email(self, request, object_id):
+    def send_test(self, request, object_id):
         email = Email.objects.get(id=object_id)
 
         call_command('softphone_email', email=email.code, audit=1)
 
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+    def send_to_user(self, request, object_id):
+        email = Email.objects.get(id=object_id)
+
+        if request.POST:
+            to = [s + '@umich.edu' for s in request.POST.get('to').split(',') ]
+            cc = request.POST.get('cc').split(',')
+            bcc = request.POST.get('bcc').split(',')
+
+            if cc != ['']:
+                cc = [s + '@umich.edu' for s in cc ]
+
+            if bcc != ['']:
+                bcc = [s + '@umich.edu' for s in bcc ]
+
+            msg = EmailMultiAlternatives(email.subject, email.subject, email.sender, to, bcc=bcc, cc=cc)
+            msg.attach_alternative(email.body, "text/html")
+            msg.send()
+
+        return TemplateResponse(
+            request,
+            'admin/project/email/ad_hoc.html',
+            {
+            'opts': self.opts,
+            'email': email,
+            }
+        )
 
 
 class TestAdmin(admin.ModelAdmin):
