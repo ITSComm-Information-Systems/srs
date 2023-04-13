292c02b3d81961fc6202845010ed669f9e8b10eb
Ryan Hoffer <rhoffer@umich.edu>
2023-04-13 11:25:00 -0400

    changed model pointer

--- reports/toll/admin.py
+++ reports/toll/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import Log
+from .models import DownloadLog
 
-admin.site.register(Log)
+admin.site.register(DownloadLog)
 
 # Register your models here.
