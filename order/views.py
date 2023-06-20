3c6420871c3b94f966f0fd2cf62a7fc10b8b1343
Ryan Hoffer <rhoffer@umich.edu>
2023-06-20 19:48:54 -0400

    moved midesktop location

--- order/views.py
+++ order/views.py
@@ -743,8 +743,10 @@ class Services(UserPassesTestMixin, View):
                                    {'label': 'View/Change Microsoft Azure', 'target': '/services/azure'},
                                    {'label': 'Order AWS', 'target': '/services/aws/add/'},
                                    {'label': 'View/Change AWS', 'target': '/services/aws/'},
-                                   {'label': 'Order MiDesktop','target': '/services/clouddesktop/add/'},
-                                   {'label': 'View/Change MiDesktop','target': '/services/clouddesktop/'} ]
+                                    ]
+            elif service.name == 'midesktop':
+                service.actions = [{'label': 'Order MiDesktop','target': '/services/clouddesktop/add/'},
+                                   {'label': 'View/Change MiDesktop','target': '/services/clouddesktop/'}]
             elif service.name == 'container':
                 service.actions = [{'label': 'Request Container Project', 'target': '/services/container/add/',
                                     'description': 'The ITS Container Service hosts containerized applications. A Container Project is a development environment for creating or hosting a containerized app.'}]
