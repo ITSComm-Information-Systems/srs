66164019b10fbe7cda8e99ed4b2dca0c78af28dc
David Jamison <djamison@umich.edu>
2023-07-17 08:18:03 -0400

    change body from literal to variable

--- project/management/commands/upload_github_billing.py
+++ project/management/commands/upload_github_billing.py
@@ -103,7 +103,7 @@ class Command(BaseCommand):
 
         email = EmailMessage(
             subject,
-            'body',
+            body,
             'srs-otto@umich.edu',
             to,
             []
