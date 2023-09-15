e88bf3d8557ba70ca943a374d6f89d2ea4bfe459
David Jamison <djamison@umich.edu>
2023-09-15 13:01:16 -0400

    prod version

--- project/management/commands/bill_audit.py
+++ project/management/commands/bill_audit.py
@@ -19,12 +19,20 @@ class Command(BaseCommand):
 
                 prev as (select count(*) as recs, sum(total_amount) as cost, data_source
                 from ps_rating.um_bill_input_api_v
-                where bill_input_file_id = %s
-                group by data_source )
+                where bill_input_file_id =  %s
+                group by data_source ),
+
+                pinn as (select decode(service_type, 'Version Ctl Hosting','GitHub',service_type) as service_type, count(*) as recs , sum(one_time_total) as cost
+                from TELECOM.ONE_TIME_CHARGE_API_V
+                where installments_remaining = 1      
+                group by service_type)
 
                 select curr.*, curr.recs - prev.recs as prev_recs, curr.cost - prev.cost as prev_cost
-                from curr, prev
-                where curr.data_source = prev.data_source
+                , curr.cost - pinn.cost as pinn_cost
+                , curr.recs - pinn.recs as pinn_recs
+                from curr
+                left join prev on curr.data_source = prev.data_source
+                left join pinn on curr.data_source = pinn.service_type
             '''
 
         today = datetime.date.today()
@@ -32,11 +40,11 @@ class Command(BaseCommand):
         last_month = today.replace(day=1) - datetime.timedelta(days=1)
         prev = last_month.strftime("%-m15%Y")
 
-        instances = get_query_result(sql, (prev, curr))
+        instances = get_query_result(sql, (curr, prev))
 
         message = render_to_string('project/billing_audit.html', {'instances': instances})
 
-        Slack().send_message(message, 'srs-errors')
+        Slack().send_message(message, 'inf-information_systems_no-interns')
 
 
 
