f1a2b52a72d6e8193068ecb9cc8e19d5cb0077e8
David Jamison <djamison@umich.edu>
2023-09-27 15:38:45 -0400

    new batch process, add send_email to Email class

---
+++ project/management/commands/shortcode_audit.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+from project.utils import get_query_result
+from project.models import Email
+import json
+
+
+class Command(BaseCommand):
+    help = 'Find expired shortcodes and send an email notice.'
+
+    def handle(self, *args, **options):
+        print('Get Email')
+
+        email = Email.objects.get(code='SHORTCODE_ONE')
+
+        sql = '''
+                    select service, owner,
+                    '[' || listagg (JSON_OBJECT (
+                        KEY 'name' IS name,
+                        KEY 'shortcode' IS shortcode)
+                        ,',') within group (order by i.name) || ']' instances
+                    from SRS_SERVICES_INSTANCES_V i
+                    left join um_mpath_accts
+                    on i.shortcode = um_mpath_accts.legacy_account
+                    where legacy_account_status <> 'O' or legacy_account_status is null
+                    group by service, owner
+        '''
+
+        for record in get_query_result(sql):
+            email.context = {"instances": json.loads(record['instances'])}
+            email.to = record['owner']
+            email.cc = 'djamison@umich.edu,djams35@gmail.com'
+            email.send()
+
+
+
+
