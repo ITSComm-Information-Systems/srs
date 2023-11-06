d2a08f812fca21de7a4437bf45e176db00a3aab1
Ryan Hoffer <rhoffer@umich.edu>
2023-11-06 14:25:02 -0500

    added name size and cidr to network list

--- services/templatetags/custom_filters.py
+++ services/templatetags/custom_filters.py
@@ -1,8 +1,13 @@
 from django import template
+import math
 
 register = template.Library()
 
-@register.filter(name='format_string')
-def format_string(value):
+@register.filter(name='format_pool_type')
+def format_pool_type(value):
     # Capitalize the first letter and replace underscores with spaces
-    return value.capitalize().replace('_', ' ')
+    return value.replace('_', ' ').title()
+
+@register.filter(name='format_cidr')
+def format_cidr(value):
+    return '/' + str(int(32 - math.log2(int(value))))
