c70db42ad494d4bb1529ab4aee9774062825ba4f
David Jamison <djamison@umich.edu>
2024-05-30 11:38:25 -0400

    add filter subscriber_status='Active'

--- order/forms.py
+++ order/forms.py
@@ -1614,7 +1614,9 @@ class AddSMSForm(forms.Form):
             self.phone_numbers = [pn['number'][2:12] for pn in zoom.get('phone_numbers')]
             loc = UmOscServiceProfileV.objects.filter(service_number__in=self.phone_numbers)
             user_depts = AuthUserDept.get_order_departments(self.request.user.id).values_list('dept', flat=True)
-            for loc in UmOscServiceProfileV.objects.filter(service_number__in=self.phone_numbers, service_type='VoIP', service_status_code='In Service'):
+            for loc in UmOscServiceProfileV.objects.filter(service_number__in=self.phone_numbers, service_type='VoIP'
+                                                           , service_status_code='In Service'
+                                                           , subscriber_status='Active'):
                 if loc.deptid not in user_depts:
                     self.add_error('uniqname', f'No order access for dept {loc.deptid}.') 
                     self.fields['uniqname'].widget.attrs.update({'class': ' is-invalid form-control'})
