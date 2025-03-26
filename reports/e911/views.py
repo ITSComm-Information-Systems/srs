59cc3fcecf20c4f72c3bb3cae601e30e591b2fab
David Jamison <djamison@umich.edu>
2025-03-26 08:48:24 -0400

    force login, close 0 records csv error

--- reports/e911/views.py
+++ reports/e911/views.py
@@ -6,12 +6,13 @@ from oscauth.models import AuthUserDept
 from project.utils import download_csv_from_queryset
 
 
+@permission_required('oscauth.can_order')
 def e911(request):
 
     department_list = list(AuthUserDept.objects.filter(user=request.user, group_id__in=[3, 4]).values_list('dept', flat=True))
     phone_numbers = ZoomAPI.objects.filter(dept_id__in=department_list, default_address=True).order_by('username').values('username','dept_id','phone_number')
 
-    if request.GET.get('file') == 'CSV':
+    if request.GET.get('file') == 'CSV' and len(phone_numbers) > 0:
         return download_csv_from_queryset(phone_numbers, file_name='e911_addresses')
 
     return render(request, 'e911.html',
