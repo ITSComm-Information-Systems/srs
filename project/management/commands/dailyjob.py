361245b31badead46bef4b31b4f6c3f7b1717a52
David  Jamison <116929499+djamison1@users.noreply.github.com>
2025-04-10 18:17:46 -0400

    Dj zoom 911 (#296)
    * stubout
    
    * v2 - UI + batch
    
    * force login, close 0 records csv error
    
    * fix fieldname default_address
    
    * text+sort
    
    * add batch to dailyjob

--- project/management/commands/dailyjob.py
+++ project/management/commands/dailyjob.py
@@ -16,6 +16,8 @@ class Command(BaseCommand):
         if date.today().weekday() < 5:
             print('weekday')
             self.weekday_jobs()
+        elif date.today().weekday() == 0:
+            self.monday_jobs()
 
         sql = '''delete from srs_auth_user_dept a
                 where group_id in (5,6) -- Orderer, Reporter
@@ -51,3 +53,16 @@ class Command(BaseCommand):
     def weekday_jobs(self):
         with connection.cursor() as cursor:
             cursor.callproc('pinn_custom.um_softphone_files_k.create_sms_file_p')
+
+    def monday_jobs(self):
+
+        call_command('zoom_api', e911='all')
+
+        sql = '''update srs_softphone_zoomapi zoom
+                SET dept_id = (SELECT max(deptid) FROM Um_Osc_Service_Profile_V 
+                                WHERE service_number = zoom.PHONE_NUMBER
+                                AND service_status_code = 'In Service'
+                                AND subscriber_status = 'Active')'''
+
+        with connection.cursor() as cursor:
+            cursor.execute(sql)                 # Set dept_id
--- reports/urls.py
+++ reports/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('metrics/', include('reports.metrics.urls')),
     path('nonteleph/', include('reports.nonteleph.urls')),
     path('usage/', include('reports.usage.urls')),
+    path('e911/', include('reports.e911.urls')),
 ]
