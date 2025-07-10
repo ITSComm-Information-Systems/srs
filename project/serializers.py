c5b3a8b5a89d85ddc47897b1e062a8f7cac4a4a8
David Jamison <djamison@umich.edu>
2025-07-10 15:24:15 -0400

    new view for admin

--- project/serializers.py
+++ project/serializers.py
@@ -33,24 +33,6 @@ class ChoiceSerializer(serializers.ModelSerializer):
         exclude = ['sequence']
 
 
-class DefaultSerializer(serializers.ModelSerializer):
-
-    def get_tickets(self, obj):
-        request = self.context.get('request')
-        if request and request.query_params.get('include_tickets') != 'true':
-            return None
-        
-        ticket_list = []
-
-        for ticket in Ticket.objects.filter(instance_id = obj.id).order_by('create_date'):
-            ticket_list.append(
-                {'ticket_id': ticket.id,
-                'instructions': ticket.data.get('misevfirewall')}
-            )
-
-        return ticket_list
-
-
 def serializer_factory(model):
     name = model.__name__
 
@@ -81,13 +63,12 @@ def serializer_factory(model):
     if model == Server:   #TODO handle children
         class_attrs['disks'] = ServerDiskSerializer(many=True, read_only=True)
         class_attrs['prefetch_related'].append('disks')
-        class_attrs['tickets'] = serializers.SerializerMethodField()
         class_attrs['tickets'] = ServerTicketSerializer(many=True, read_only=True)
         class_attrs['prefetch_related'].append('tickets')
     elif model == Image:
         class_attrs['storage'] = ImageDiskSerializer(many=True, read_only=True)
 
-    return type(f'{name}Serializer', (DefaultSerializer,), class_attrs)
+    return type(f'{name}Serializer', (serializers.ModelSerializer,), class_attrs)
 
 class DatabaseSerializer(serializers.ModelSerializer):
 
