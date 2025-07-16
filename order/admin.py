fab39e094b3a91fcd3cd94bad8c4fe3d6bb6a1d9
David Jamison <djamison@umich.edu>
2025-07-16 14:34:40 -0400

    v1

--- order/admin.py
+++ order/admin.py
@@ -250,6 +250,7 @@ class ServerAdmin(ServiceInstanceAdmin):
             'fields': (('name', 'in_service','created_date'), 'owner', ('admin_group','database_type'), 'shortcode', ('os','cpu','ram'),
                         ('replicated','backup','backup_time','public_facing','production'),
                         ('on_call', 'support_email', 'support_phone'),
+                        ('cname',),
 
                         ('regulated_data','non_regulated_data'),
                         'managed'
